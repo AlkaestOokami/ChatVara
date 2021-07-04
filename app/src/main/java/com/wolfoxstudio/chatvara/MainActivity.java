f15e41a073cb251ee6121146593cd610bcfa26a0
Yann Cordeiro dos santos <45500604+AlkaestOokami@users.noreply.github.com>
2021-07-04 04:23:16 -0300

    Feature: Tela de login adicionada.
    Design de tela de login incluida, bem como eventos de click para transição de tela.

--- app/src/main/java/com/wolfoxstudio/chatvara/MainActivity.java
+++ app/src/main/java/com/wolfoxstudio/chatvara/MainActivity.java
@@ -2,13 +2,48 @@ package com.wolfoxstudio.chatvara;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editEmail;
+    private EditText editPassword;
+    private Button btnEnter;
+    private TextView txtNewAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editEmail = findViewById(R.id.edit_email);
+        editPassword = findViewById(R.id.edit_password);
+        btnEnter = findViewById(R.id.button_login);
+        txtNewAccount = findViewById(R.id.txt_new_account02);
+
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editEmail.getText().toString();
+                String password = editPassword.getText().toString();
+
+                Log.i("Test", email);
+                Log.i("Test", password);
+            }
+        });
+
+        txtNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
